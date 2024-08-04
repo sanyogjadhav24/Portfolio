0ed648d7924f74ab27319f80ef2038a29d9c88ae
sanyogjadhav24 <jadhavsanyog.400@gmail.com>
2024-08-04 18:21:58 +0530

    changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container">
-        <a className="navbar-brand" href="#">SanyogTech</a>
+        <a className="navbar-brand" href="/">SanyogTech</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
