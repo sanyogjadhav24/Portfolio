a3221fb13b876448d8b5b0127aac929ed1c24392
sanyogjadhav24 <jadhavsanyog.400@gmail.com>
2024-08-05 23:23:02 +0530

    c6

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,25 +7,25 @@ const Footer = () => {
       <div className="container">
         <div className="row">
           <div className="col-md-4">
-          <center>  <h5>Contact</h5>
-          <p>Email: jadhavsanyog.400@gmail.com</p>
-          <p>Phone:+91 8600596593</p></center>
+            <h5>Contact</h5>
+       
+          <p>Phone:+91 8600596593</p>
           </div>
           <div className="col-md-4">
-          <center>    <h5>Quick Links</h5>
+        <h5>Quick Links</h5>
          <ul className="list-unstyled">
               <li><a href="#projects">Projects</a></li>
               <li><a href="#about">About</a></li>
               <li><a href="#contact">Contact</a></li>
-            </ul></center> 
+            </ul>
           </div>
           <div className="col-md-4">
-          <center>   <h5>Follow Me</h5>
+   <h5>Follow Me</h5>
             <ul className="list-unstyled social-icons">
               <li><a href="https://github.com/sanyogjadhav24" target="_blank" rel="noopener noreferrer"><i className="fab fa-github"></i></a></li>
               <li><a href="https://linkedin.com/in/sanyog-jadhav-295154258" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin"></i></a></li>
               <li><a href="https://instagram.com/sanyog_jadhav_2424" target="_blank" rel="noopener noreferrer"><i className="fab fa-instagram"></i></a></li>
-            </ul></center> 
+            </ul>
           </div>
         </div>
         <div className="text-center mt-4">
