4f19c3b436f0abb104db200deeda4909cd199074
sanyogjadhav24 <jadhavsanyog.400@gmail.com>
2024-08-06 14:27:49 +0530

    c9

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -58,7 +58,7 @@ const Projects = () => {
                   <img src={project1} className="card-img-top" alt="Project 1" />
                   <div className="card-body">
                     <h5 className="card-title">Rock Paper Scissors</h5>
-                    <p className="card-text">JavaScript Based Project which is a Very Popular Game called Rock-Paper-Scissors (Language Used: HTML, CSS, JS)</p>
+                    <p className="card-text">JavaScript Based Project which is a Very Popular Game called Rock-Paper-Scissors (Language Used: HTML, CSS, JS).</p>
                     <Link to="/project/rock-paper-scissors" className="btn btn-custom">View Details</Link>
                   </div>
                 </div>
@@ -68,7 +68,7 @@ const Projects = () => {
                   <img src={project2} className="card-img-top" alt="Project 2" />
                   <div className="card-body">
                     <h5 className="card-title">Tic-Tac-Toe</h5>
-                    <p className="card-text">JavaScript Based Project which is a Very Popular Game called Tic-Tac-Toe also called O and X (Language Used: HTML, CSS, JS)</p>
+                    <p className="card-text">JavaScript Based Project which is a Very Popular Game called Tic-Tac-Toe also called O and X (Language Used: HTML, CSS, JS).</p>
                     <Link to="/project/tic-tac-toe" className="btn btn-custom">View Details</Link>
                   </div>
                 </div>
@@ -78,7 +78,7 @@ const Projects = () => {
                   <img src={project3} className="card-img-top" alt="Project 3" />
                   <div className="card-body">
                     <h5 className="card-title">Currency Converter</h5>
-                    <p className="card-text">JavaScript Based Project, This Website helps us to convert the Currency in different Countries. (Language Used: HTML, CSS, JS)</p>
+                    <p className="card-text">JavaScript Based Project, This Website helps us to convert the Currency in different Countries. (Language Used: HTML, CSS, JS).</p>
                     <Link to="/project/currency-converter" className="btn btn-custom">View Details</Link>
                   </div>
                 </div>
@@ -96,7 +96,7 @@ const Projects = () => {
                   <img src={react1} className="card-img-top" alt="Project 1" />
                   <div className="card-body">
                     <h5 className="card-title">WordCount Pro</h5>
-                    <p className="card-text">WordCount Pro is a versatile text analysis tool designed to enhance your writing experience.(Library Used:React Js).</p>
+                    <p className="card-text">WordCount Pro is a versatile text analysis tool designed to enhance your writing experience. (Library Used: React JS).</p>
                     <Link to="/project/wordcount-pro" className="btn btn-custom">View Details</Link>
                   </div>
                 </div>
@@ -106,7 +106,7 @@ const Projects = () => {
                   <img src={video} className="card-img-top" alt="Project 2" />
                   <div className="card-body">
                     <h5 className="card-title">Meet Here</h5>
-                    <p className="card-text">Meet Here is Video Conferencing App which allows user to make a video call. (Framework Used: React Native)</p>
+                    <p className="card-text">Meet Here is a Video Conferencing App which allows users to make video calls. (Framework Used: React Native).</p>
                     <Link to="/project/meet-here" className="btn btn-custom">View Details</Link>
                   </div>
                 </div>
@@ -116,7 +116,7 @@ const Projects = () => {
                   <img src={Rf} className="card-img-top" alt="Project 3" />
                   <div className="card-body">
                     <h5 className="card-title">Registration Form</h5>
-                    <p className="card-text">Registration Form is a Simple form that allow user to fill the information and submit it. (Library Used: React JS)</p>
+                    <p className="card-text">Registration Form is a simple form that allows users to fill in information and submit it. (Library Used: React JS).</p>
                     <Link to="/project/registration-form" className="btn btn-custom">View Details</Link>
                   </div>
                 </div>
@@ -134,7 +134,7 @@ const Projects = () => {
                   <img src={expense} className="card-img-top" alt="Project 1" />
                   <div className="card-body">
                     <h5 className="card-title">Expense Tracker</h5>
-                    <p className="card-text">Expense Tracker is Android Application which allows user to track his daily and monthly expenses.(Language Used:Java and Xml).</p>
+                    <p className="card-text">Expense Tracker is an Android application that allows users to track their daily and monthly expenses. (Language Used: Java and XML).</p>
                     <Link to="/project/expense-tracker" className="btn btn-custom">View Details</Link>
                   </div>
                 </div>
@@ -144,7 +144,7 @@ const Projects = () => {
                   <img src={quiz} className="card-img-top" alt="Project 2" />
                   <div className="card-body">
                     <h5 className="card-title">Quiz Game</h5>
-                    <p className="card-text">Quiz Game allows User to select his Interest and allows to play a Quiz based on Interest Choosen by User.(Language Used:java and Xml) </p>
+                    <p className="card-text">Quiz Game allows users to select their interests and play a quiz based on their chosen interests. (Language Used: Java and XML). </p>
                     <Link to="/project/quiz-game" className="btn btn-custom">View Details</Link>
                   </div>
                 </div>
@@ -154,7 +154,7 @@ const Projects = () => {
                   <img src={signup} className="card-img-top" alt="Project 3" />
                   <div className="card-body">
                     <h5 className="card-title">SignUp and Login </h5>
-                    <p className="card-text">Sign Up and Login Page allows user to login the app here proper Authentication is done.(Language Used: Java,Xml,Firebase)</p>
+                    <p className="card-text">Sign Up and Login Page allows users to log in to the app with proper authentication. (Language Used: Java, XML, Firebase).</p>
                     <Link to="/project/signup-login" className="btn btn-custom">View Details</Link>
                   </div>
                 </div>
@@ -172,7 +172,7 @@ const Projects = () => {
                   <img src={guess} className="card-img-top" alt="Project 1" />
                   <div className="card-body">
                     <h5 className="card-title">Guess The word</h5>
-                    <p className="card-text">Guess the Word is Python based game that able user to guess the words of different things.(Language Used: Python)</p>
+                    <p className="card-text">Guess the Word is a Python-based game that allows users to guess words for different things. (Language Used: Python)</p>
                     <Link to="/project/guess-the-word" className="btn btn-custom">View Details</Link>
                   </div>
                 </div>
@@ -182,7 +182,7 @@ const Projects = () => {
                   <img src={suduku} className="card-img-top" alt="Project 2" />
                   <div className="card-body">
                     <h5 className="card-title">Soduku Solver Pro</h5>
-                    <p className="card-text">Soduku Solver is the Python Based Project Whic solver any soduku that is given as Input(Language Used: Python)</p>
+                    <p className="card-text">Soduku Solver is a Python-based project that solves any Sudoku puzzle given as input. (Language Used: Python).</p>
                     <Link to="/project/soduku-solver" className="btn btn-custom">View Details</Link>
                   </div>
                 </div>
@@ -192,7 +192,7 @@ const Projects = () => {
                   <img src={calc} className="card-img-top" alt="Project 3" />
                   <div className="card-body">
                     <h5 className="card-title">Scientific Calculator</h5>
-                    <p className="card-text">Scientific Calculator is the Python based Calculator,The GUI is developed Using TKinter.(Language Used:Python)</p>
+                    <p className="card-text">Scientific Calculator is a Python-based calculator with a graphical user interface developed using Tkinter. (Language Used: Python)</p>
                     <Link to="/project/scientific-calculator" className="btn btn-custom">View Details</Link>
                   </div>
                 </div>
